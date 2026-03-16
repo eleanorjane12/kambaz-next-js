9dec18be7ea1a1484843dbf3a39462fc1c79d0ce
eleanorjane12 <collins.eleanor.j@gmail.com>
2026-03-16 19:50:53 -0400

    a4 Redux

--- app/(kambaz)/account/Navigation.tsx
+++ app/(kambaz)/account/Navigation.tsx
@@ -1,8 +1,10 @@
+"use client"
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { Nav, NavItem, NavLink } from "react-bootstrap";
 import { useSelector } from "react-redux";
 import { RootState } from "../store";
+
 export default function AccountNavigation() {
   const { currentUser } = useSelector((state: RootState) => state.accountReducer);
   const links = currentUser ? ["profile"] : ["signin", "signup"];
