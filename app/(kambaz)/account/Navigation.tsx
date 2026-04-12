5ac9f8c25d114b54666e779a58e90086f26e673d
eleanorjane12 <collins.eleanor.j@gmail.com>
2026-04-12 18:30:46 -0400

    nav fixed

--- app/(kambaz)/account/Navigation.tsx
+++ app/(kambaz)/account/Navigation.tsx
@@ -6,7 +6,7 @@ import { useSelector } from "react-redux";
 import { RootState } from "../store";
 
 export default function AccountNavigation() {
-  const { currentUser } = useSelector((state: RootState) => state.accountReducer);
+  const { currentUser } = useSelector((state: RootState) => state.accountReducer) as { currentUser: { role: string } | null };
   const links = currentUser ? ["profile"] : ["signin", "signup"];
   const pathname = usePathname();
 
